85d30d6fb6d23bb1ad95103bae01742394c0c15e
Thanhsangg <thanhsang6325@gmail.com>
2021-04-01 16:52:01 +0700

    Luu tru bai tap

---
+++ Security.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace BaiTap.Net
+{
+    partial class Security
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Security));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LB1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BT0 = new System.Windows.Forms.Button();
+            this.BT9 = new System.Windows.Forms.Button();
+            this.BT8 = new System.Windows.Forms.Button();
+            this.BT7 = new System.Windows.Forms.Button();
+            this.BTEnter = new System.Windows.Forms.Button();
+            this.BT6 = new System.Windows.Forms.Button();
+            this.BT5 = new System.Windows.Forms.Button();
+            this.BT4 = new System.Windows.Forms.Button();
+            this.BTBackspace = new System.Windows.Forms.Button();
+            this.BT3 = new System.Windows.Forms.Button();
+            this.BT2 = new System.Windows.Forms.Button();
+            this.BT1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBCode = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.LB1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.BT0);
+            this.panel1.Controls.Add(this.BT9);
+            this.panel1.Controls.Add(this.BT8);
+            this.panel1.Controls.Add(this.BT7);
+            this.panel1.Controls.Add(this.BTEnter);
+            this.panel1.Controls.Add(this.BT6);
+            this.panel1.Controls.Add(this.BT5);
+            this.panel1.Controls.Add(this.BT4);
+            this.panel1.Controls.Add(this.BTBackspace);
+            this.panel1.Controls.Add(this.BT3);
+            this.panel1.Controls.Add(this.BT2);
+            this.panel1.Controls.Add(this.BT1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.TBCode);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(330, 408);
+            this.panel1.TabIndex = 0;
+            // 
+            // LB1
+            // 
+            this.LB1.FormattingEnabled = true;
+            this.LB1.Location = new System.Drawing.Point(13, 235);
+            this.LB1.Name = "LB1";
+            this.LB1.Size = new System.Drawing.Size(305, 160);
+            this.LB1.TabIndex = 31;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(21, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Access log:";
+            // 
+            // BT0
+            // 
+            this.BT0.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT0.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT0.Location = new System.Drawing.Point(221, 156);
+            this.BT0.Name = "BT0";
+            this.BT0.Size = new System.Drawing.Size(50, 44);
+            this.BT0.TabIndex = 29;
+            this.BT0.Text = "0";
+            this.BT0.UseVisualStyleBackColor = false;
+            this.BT0.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT9
+            // 
+            this.BT9.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT9.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT9.Location = new System.Drawing.Point(165, 156);
+            this.BT9.Name = "BT9";
+            this.BT9.Size = new System.Drawing.Size(50, 44);
+            this.BT9.TabIndex = 28;
+            this.BT9.Text = "9";
+            this.BT9.UseVisualStyleBackColor = false;
+            this.BT9.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT8
+            // 
+            this.BT8.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT8.Location = new System.Drawing.Point(109, 156);
+            this.BT8.Name = "BT8";
+            this.BT8.Size = new System.Drawing.Size(50, 44);
+            this.BT8.TabIndex = 27;
+            this.BT8.Text = "8";
+            this.BT8.UseVisualStyleBackColor = false;
+            this.BT8.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT7
+            // 
+            this.BT7.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT7.Location = new System.Drawing.Point(53, 156);
+            this.BT7.Name = "BT7";
+            this.BT7.Size = new System.Drawing.Size(50, 44);
+            this.BT7.TabIndex = 26;
+            this.BT7.Text = "7";
+            this.BT7.UseVisualStyleBackColor = false;
+            this.BT7.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BTEnter
+            // 
+            this.BTEnter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BTEnter.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTEnter.BackgroundImage")));
+            this.BTEnter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTEnter.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTEnter.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTEnter.Location = new System.Drawing.Point(221, 106);
+            this.BTEnter.Name = "BTEnter";
+            this.BTEnter.Size = new System.Drawing.Size(50, 44);
+            this.BTEnter.TabIndex = 25;
+            this.BTEnter.UseVisualStyleBackColor = false;
+            this.BTEnter.Click += new System.EventHandler(this.BTEnter_Click);
+            // 
+            // BT6
+            // 
+            this.BT6.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT6.Location = new System.Drawing.Point(165, 106);
+            this.BT6.Name = "BT6";
+            this.BT6.Size = new System.Drawing.Size(50, 44);
+            this.BT6.TabIndex = 24;
+            this.BT6.Text = "6";
+            this.BT6.UseVisualStyleBackColor = false;
+            this.BT6.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT5
+            // 
+            this.BT5.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT5.Location = new System.Drawing.Point(109, 106);
+            this.BT5.Name = "BT5";
+            this.BT5.Size = new System.Drawing.Size(50, 44);
+            this.BT5.TabIndex = 23;
+            this.BT5.Text = "5";
+            this.BT5.UseVisualStyleBackColor = false;
+            this.BT5.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT4
+            // 
+            this.BT4.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT4.Location = new System.Drawing.Point(53, 106);
+            this.BT4.Name = "BT4";
+            this.BT4.Size = new System.Drawing.Size(50, 44);
+            this.BT4.TabIndex = 22;
+            this.BT4.Text = "4";
+            this.BT4.UseVisualStyleBackColor = false;
+            this.BT4.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BTBackspace
+            // 
+            this.BTBackspace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BTBackspace.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTBackspace.BackgroundImage")));
+            this.BTBackspace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BTBackspace.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTBackspace.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTBackspace.Location = new System.Drawing.Point(221, 56);
+            this.BTBackspace.Name = "BTBackspace";
+            this.BTBackspace.Size = new System.Drawing.Size(50, 44);
+            this.BTBackspace.TabIndex = 21;
+            this.BTBackspace.UseVisualStyleBackColor = false;
+            this.BTBackspace.Click += new System.EventHandler(this.BTBackspace_Click);
+            // 
+            // BT3
+            // 
+            this.BT3.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT3.Location = new System.Drawing.Point(165, 56);
+            this.BT3.Name = "BT3";
+            this.BT3.Size = new System.Drawing.Size(50, 44);
+            this.BT3.TabIndex = 20;
+            this.BT3.Text = "3";
+            this.BT3.UseVisualStyleBackColor = false;
+            this.BT3.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT2
+            // 
+            this.BT2.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT2.Location = new System.Drawing.Point(109, 56);
+            this.BT2.Name = "BT2";
+            this.BT2.Size = new System.Drawing.Size(50, 44);
+            this.BT2.TabIndex = 19;
+            this.BT2.Text = "2";
+            this.BT2.UseVisualStyleBackColor = false;
+            this.BT2.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT1
+            // 
+            this.BT1.BackColor = System.Drawing.Color.RosyBrown;
+            this.BT1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BT1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT1.Location = new System.Drawing.Point(53, 56);
+            this.BT1.Name = "BT1";
+            this.BT1.Size = new System.Drawing.Size(50, 44);
+            this.BT1.TabIndex = 18;
+            this.BT1.Text = "1";
+            this.BT1.UseVisualStyleBackColor = false;
+            this.BT1.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(21, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Security Code:";
+            // 
+            // TBCode
+            // 
+            this.TBCode.BackColor = System.Drawing.Color.Black;
+            this.TBCode.ForeColor = System.Drawing.Color.White;
+            this.TBCode.Location = new System.Drawing.Point(103, 13);
+            this.TBCode.MaxLength = 4;
+            this.TBCode.Name = "TBCode";
+            this.TBCode.PasswordChar = '♦';
+            this.TBCode.Size = new System.Drawing.Size(187, 20);
+            this.TBCode.TabIndex = 16;
+            this.TBCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TBCode_KeyPress);
+            // 
+            // Security
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(330, 407);
+            this.Controls.Add(this.panel1);
+            this.Name = "Security";
+            this.Text = "123";
+            this.Load += new System.EventHandler(this.Security_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox LB1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BT0;
+        private System.Windows.Forms.Button BT9;
+        private System.Windows.Forms.Button BT8;
+        private System.Windows.Forms.Button BT7;
+        private System.Windows.Forms.Button BTEnter;
+        private System.Windows.Forms.Button BT6;
+        private System.Windows.Forms.Button BT5;
+        private System.Windows.Forms.Button BT4;
+        private System.Windows.Forms.Button BTBackspace;
+        private System.Windows.Forms.Button BT3;
+        private System.Windows.Forms.Button BT2;
+        private System.Windows.Forms.Button BT1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBCode;
+    }
+}
+
---
+++ Security.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap.Net
+{
+    public partial class Security : Form
+    {
+        public Security()
+        {
+            InitializeComponent();
+        }
+
+        private void BT1_Click(object sender, EventArgs e)
+        {
+            if(TBCode.Text.Length < 4)
+            {
+                TBCode.Text += ((Button)sender).Text;
+            }
+        }
+
+        //private void BT2_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT2.Text;
+        //}
+
+        //private void BT3_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT3.Text;
+        //}
+
+        //private void BT4_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT4.Text;
+        //}
+
+        //private void BT5_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT5.Text;
+        //}
+
+        //private void BT6_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT6.Text;
+        //}
+
+        //private void BT7_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT7.Text;
+        //}
+
+        //private void BT8_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT8.Text;
+        //}
+
+        //private void BT9_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT9.Text;
+        //}
+
+        //private void BT0_Click(object sender, EventArgs e)
+        //{
+        //    TBCode.PasswordChar = '*';
+        //    TBCode.Text += BT0.Text;
+        //}
+
+        private void BTBackspace_Click(object sender, EventArgs e)
+        {
+            if(TBCode.Text.Length == 0)
+            {
+                return;
+            }
+            else
+            {
+                TBCode.Text = TBCode.Text.Remove(TBCode.Text.Length - 1);
+            }
+        }
+
+        private void BTEnter_Click(object sender, EventArgs e)
+        {
+            string t;
+            if (TBCode.Text == "1806")
+            {
+                string path = "E:\\.Net\\BaiTap.Net\\102190037.txt";
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Write);
+                fs.Position = fs.Length;
+                StreamWriter sw = new StreamWriter(fs);
+                t = " ❉  " + DateTime.Now.ToString() + "                    " + "Access Granted";
+                sw.WriteLine(" ❉  " + DateTime.Now.ToString() + "                    " + "Access Granted");
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            else if(TBCode.Text.Length == 1)
+            {
+                string path = "E:\\.Net\\BaiTap.Net\\102190037.txt";
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Write);
+                fs.Position = fs.Length;
+                StreamWriter sw = new StreamWriter(fs);
+                t = " ✥  " + DateTime.Now.ToString() + "                    " + "Restricted Access";
+                sw.WriteLine(" ✥  " + DateTime.Now.ToString() + "                    " + "Restricted Access");
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            else
+            {
+                string path = "E:\\.Net\\BaiTap.Net\\102190037.txt";
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Write);
+                fs.Position = fs.Length;
+                StreamWriter sw = new StreamWriter(fs);
+                t = " ✤  " + DateTime.Now.ToString() + "                    " + "Access Denied";
+                sw.WriteLine(" ✤  " + DateTime.Now.ToString() + "                    " + "Access Denied");
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            LB1.Items.Add(t);
+            TBCode.Clear();
+        }
+
+        private void TBCode_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            } 
+            if(e.KeyChar == (Char)Keys.Enter) // hoặc e.KeyChar == (Char)13 == Convert.ToInt32(e.KeyChar)
+            {
+                string t;
+                if (TBCode.Text == "1806")
+                {
+                    string path = "E:\\.Net\\BaiTap.Net\\102190037.txt";
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Write);
+                    fs.Position = fs.Length;
+                    StreamWriter sw = new StreamWriter(fs);
+                    t = " ❉  " + DateTime.Now.ToString() + "                    " + "Access Granted";
+                    sw.WriteLine(" ❉  " + DateTime.Now.ToString() + "                    " + "Access Granted");
+                    sw.Flush();
+                    sw.Close();
+                    fs.Close();
+                }
+                else if (TBCode.Text.Length == 1)
+                {
+                    string path = "E:\\.Net\\BaiTap.Net\\102190037.txt";
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Write);
+                    fs.Position = fs.Length;
+                    StreamWriter sw = new StreamWriter(fs);
+                    t = " ✥  " + DateTime.Now.ToString() + "                    " + "Restricted Access";
+                    sw.WriteLine(" ✥  " + DateTime.Now.ToString() + "                    " + "Restricted Access");
+                    sw.Flush();
+                    sw.Close();
+                    fs.Close();
+                }
+                else
+                {
+                    string path = "E:\\.Net\\BaiTap.Net\\102190037.txt";
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Write);
+                    fs.Position = fs.Length;
+                    StreamWriter sw = new StreamWriter(fs);
+                    t = " ✤  " + DateTime.Now.ToString() + "                    " + "Access Denied";
+                    sw.WriteLine(" ✤  " + DateTime.Now.ToString() + "                    " + "Access Denied");
+                    sw.Flush();
+                    sw.Close();
+                    fs.Close();
+                }
+                LB1.Items.Add(t);
+                TBCode.Clear();
+            }    
+        }
+
+        private void Security_Load(object sender, EventArgs e)
+        {
+            string path = "E:\\.Net\\BaiTap.Net\\102190037.txt";
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string t;
+            while((t = sr.ReadLine()) != null)
+            {
+                LB1.Items.Add(t);
+            }   
+            sr.Close();
+            fs.Close();
+        }
+
+    }
+}
